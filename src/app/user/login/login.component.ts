b683f13ce5c1deaaa83a4ebae8047112fc5184ce
Jonathan Hermansen <jhermansen@live.no>
2023-02-27 17:30:09 +0100

    Implements login logic

--- src/app/user/login/login.component.ts
+++ src/app/user/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-login',
@@ -10,9 +11,35 @@ export class LoginComponent {
     email: '',
     password: ''
   };
+  showAlert = false;
+  alertMsg = 'Please wait! We are logging you in.';
+  alertColor = 'blue';
+  inSubmission = false;
 
-  login() {
-    console.log('Logged in.')
-    console.log(this.credentials)
+  constructor(private auth: AngularFireAuth) {
+  }
+
+  async login() {
+    this.showAlert = true;
+    this.alertMsg = 'Please wait! We are logging you in.';
+    this.alertColor = 'blue';
+    this.inSubmission = true;
+
+    try {
+      await this.auth.signInWithEmailAndPassword(
+        this.credentials.email, this.credentials.password
+      );
+    } catch(e) {
+      this.showAlert = false;
+      this.alertMsg = 'An unexprected error occurred. Please try again.';
+      this.alertColor = 'red';
+
+      console.log(e);
+
+      return;
+    }
+
+    this.alertMsg = 'Success! You are now logged in.';
+    this.alertColor = 'green';
   }
 }
