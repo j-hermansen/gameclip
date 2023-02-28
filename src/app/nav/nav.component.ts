ea171ece25327bd84bd43ef2f64c1191b40bd51c
Jonathan Hermansen <jhermansen@live.no>
2023-02-28 01:11:55 +0100

    Logout button in main menu

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { ModalService } from "../services/modal.service";
 import { AuthService } from '../services/auth.service';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 
 @Component({
   selector: 'app-nav',
@@ -9,7 +10,7 @@ import { AuthService } from '../services/auth.service';
 })
 export class NavComponent {
 
-  constructor(public modal: ModalService, public auth: AuthService) {
+  constructor(public modal: ModalService, public auth: AuthService, public afAuth: AngularFireAuth) {
   }
 
   openModal($event: Event) {
@@ -17,5 +18,11 @@ export class NavComponent {
 
     this.modal.toggleModal('auth');
 
+  }
+
+  async logout($event: Event) {
+    $event.preventDefault();
+
+    await this.afAuth.signOut();
   }
 }
