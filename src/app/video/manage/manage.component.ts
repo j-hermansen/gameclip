71963331ffb8ebc8a6a260babead69d0d53fa58d
Jonathan Hermansen <jhermansen@live.no>
2023-03-08 14:51:18 +0100

    Update edit modal to contain title of clip

--- src/app/video/manage/manage.component.ts
+++ src/app/video/manage/manage.component.ts
@@ -12,6 +12,7 @@ import { ModalService } from '../../services/modal.service';
 export class ManageComponent implements OnInit {
   videoOrder = '1';
   clips: IClip[] = [];
+  activeClip: IClip | null = null;
 
   constructor(private router: Router, private route: ActivatedRoute, private clipService: ClipService, private modal: ModalService) {
   }
@@ -48,6 +49,8 @@ export class ManageComponent implements OnInit {
 
   openModal($event: Event, clip: IClip) {
     $event.preventDefault();
+
+    this.activeClip = clip;
 
     this.modal.toggleModal('editClip');
   }
